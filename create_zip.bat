f7ac626f0bd58d77b78154fb50a114300ac75493
clbustos <clbustos@gmail.com>
2018-09-09 07:21:12 -0300

    Fixed bug on Buhos-1.0.0-beta15

---
+++ create_zip.bat
@@ -0,0 +1,16 @@
+
+  del /f /s /q ..\buhos_zip > nul
+  rmdir /s /q ..\buhos_zip
+
+git clone . ../buhos_zip
+git clone ../buhos ../buhos_zip/lib/buhos
+rmdir /s /q  ..\buhos_zip\windows_installer
+rmdir /s /q ..\buhos_zip\.git
+rmdir /s /q ..\buhos_zip\lib\buhos\.git
+del ..\buhos_zip\*.rb
+del ..\buhos_zip\*.xcf
+del ..\buhos_zip\*.iss
+del ..\buhos_zip\*.sh
+del ..\buhos_zip\*.erb
+del ..\buhos_zip\.gitattributes
+del ..\buhos_zip\.gitignore
